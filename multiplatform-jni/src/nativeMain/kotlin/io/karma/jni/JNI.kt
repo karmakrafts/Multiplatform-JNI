8cd06f32a2654d9b9f6ab9544e5c5f9c900dfcb9
KitsuneAlex <alexh@karmakrafts.dev>
2024-09-25 02:41:18 +0200

    Implement missing string conversions and jniScoped

--- multiplatform-jni/src/nativeMain/kotlin/io/karma/jni/JNI.kt
+++ multiplatform-jni/src/nativeMain/kotlin/io/karma/jni/JNI.kt
@@ -1,10 +1,13 @@
-@file:OptIn(ExperimentalForeignApi::class)
+@file:OptIn(ExperimentalForeignApi::class, ExperimentalNativeApi::class)
 
 package io.karma.jni
 
 import jni.JNIEnvVar
+import jni.JNI_ERR
 import jni.JNI_FALSE
 import jni.JNI_TRUE
+import jni.JNI_VERSION_1_8
+import jni.JavaVMVar
 import jni.jbyte
 import jni.jclass
 import jni.jdouble
@@ -15,6 +18,7 @@ import jni.jlong
 import jni.jmethodID
 import jni.jobject
 import jni.jshort
+import jni.jstring
 import jni.jvalue
 import kotlinx.cinterop.ByteVar
 import kotlinx.cinterop.COpaquePointer
@@ -23,19 +27,102 @@ import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.MemScope
 import kotlinx.cinterop.addressOf
 import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.allocPointerTo
 import kotlinx.cinterop.convert
+import kotlinx.cinterop.interpretCPointer
 import kotlinx.cinterop.invoke
 import kotlinx.cinterop.memScoped
 import kotlinx.cinterop.pointed
 import kotlinx.cinterop.ptr
+import kotlinx.cinterop.reinterpret
+import kotlinx.cinterop.toKString
 import kotlinx.cinterop.usePinned
 import platform.posix.memcpy
+import kotlin.concurrent.AtomicNativePtr
+import kotlin.experimental.ExperimentalNativeApi
+import kotlin.native.concurrent.ThreadLocal
+import kotlin.native.internal.NativePtr
+
+private val virtualMachineAddress: AtomicNativePtr = AtomicNativePtr(NativePtr.NULL)
+var virtualMachine: JavaVMVar
+    get() = interpretCPointer<JavaVMVar>(virtualMachineAddress.value)!!.pointed
+    set(value) {
+        virtualMachineAddress.value = value.rawPtr
+    }
+
+@ThreadLocal
+var environment: JNIEnvVar = interpretCPointer<JNIEnvVar>(NativePtr.NULL)!!.pointed
+
+@Suppress("UNUSED_PARAMETER", "UNUSED")
+@CName("JNI_OnLoad")
+fun jniOnLoad(vm: JavaVMVar, reserved: COpaquePointer): jint {
+    virtualMachine = vm
+    return vm.pointed?.let {
+        memScoped {
+            val address = allocPointerTo<JNIEnvVar>()
+            requireNotNull(it.GetEnv)(
+                vm.ptr,
+                address.ptr.reinterpret(),
+                JNI_VERSION_1_8
+            )
+            environment = requireNotNull(address.pointed)
+        }
+        return@let JNI_VERSION_1_8
+    } ?: JNI_ERR
+}
+
+value class JNIEnvScope(val environment: JNIEnvVar) {
+    inline fun jstring.toKString(): String = toKString(environment)
+    inline fun String.toJString(): jstring = toJString(environment)
+
+    inline fun jclass.findField(descriptor: FieldDescriptor): jfieldID? =
+        findField(environment, descriptor)
+
+    inline fun jclass.findMethod(descriptor: MethodDescriptor): jmethodID? =
+        findMethod(environment, descriptor)
+
+    inline operator fun <reified R> jmethodID.invoke(instance: jobject, vararg args: Any?): R? {
+        return invoke<R>(environment, instance, *args)
+    }
+}
+
+inline fun <reified R> jniScoped(scope: JNIEnvScope.() -> R): R {
+    return scope(JNIEnvScope(environment))
+}
 
 fun MemScope.allocCString(value: String): CPointer<ByteVar> {
     return value.encodeToByteArray().let { bytes ->
         val data = allocArray<ByteVar>(bytes.size)
         bytes.usePinned { memcpy(data, it.addressOf(0), bytes.size.convert()) }
         data
+    }
+}
+
+fun jstring.toKString(env: JNIEnvVar): String {
+    val data = requireNotNull(
+        requireNotNull(env.pointed?.GetStringUTFChars) { "Could not access JNI:GetStringUTFChars function" }(
+            env.ptr,
+            this@toKString,
+            null
+        )
+    ) { "Could not retrieve string base address" }
+    val result = data.toKString()
+    requireNotNull(env.pointed?.ReleaseStringUTFChars) { "Could not access JNI:ReleaseStringUTFChars function" }(
+        env.ptr,
+        this@toKString,
+        data
+    )
+    return result
+}
+
+fun String.toJString(env: JNIEnvVar): jstring {
+    return memScoped {
+        requireNotNull(
+            env.pointed?.NewStringUTF?.invoke(
+                env.ptr,
+                allocCString(this@toJString)
+            )
+        ) { "Could not allocate JVM string" }
     }
 }
 
@@ -85,8 +172,8 @@ inline operator fun <reified R> jmethodID.invoke(
                 is Short, is UShort -> value as jshort
                 is Int, is UInt -> value as jint
                 is Long, is ULong -> value as jlong
-                is Float -> value as jfloat
-                is Double -> value as jdouble
+                is Float -> value
+                is Double -> value
                 is Boolean -> if (value) JNI_TRUE else JNI_FALSE
                 is CPointer<*> -> value.rawValue.toLong().convert<jlong>()
                 is COpaquePointer -> value.rawValue.toLong().convert<jlong>()
