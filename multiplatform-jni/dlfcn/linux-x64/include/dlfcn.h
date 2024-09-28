9898b8d542ff971b9c1475c8b885b95a6036f4b3
KitsuneAlex <alexh@karmakrafts.dev>
2024-09-28 23:02:33 +0200

    Inline macro definitions for RTLD_ flags on linux

--- multiplatform-jni/dlfcn/linux-x64/include/dlfcn.h
+++ multiplatform-jni/dlfcn/linux-x64/include/dlfcn.h
@@ -23,9 +23,12 @@
 #define __need_size_t
 #include <stddef.h>
 
-/* Collect various system dependent definitions and declarations.  */
-#include <bits/dlfcn.h>
-
+/* The MODE argument to `dlopen' contains one of the following: */
+#define RTLD_LAZY	0x00001	/* Lazy function call binding.  */
+#define RTLD_NOW	0x00002	/* Immediate function call binding.  */
+#define	RTLD_BINDING_MASK   0x3	/* Mask of binding time value.  */
+#define RTLD_NOLOAD	0x00004	/* Do not load the object.  */
+#define RTLD_DEEPBIND	0x00008	/* Use deep binding.  */
 
 #ifdef __USE_GNU
 #include <bits/dl_find_object.h>
